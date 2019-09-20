eb775bc1da91b1298a486caf6a892dbef1e69058
florpi <forpisoteadadejardin@gmail.com>
2019-09-20 16:45:26 +0100

    added exploration of new dataset

--- inputs/generate_graph.py
+++ inputs/generate_graph.py
@@ -28,7 +28,7 @@ def reformat_edges_distances(edges, distances):
 	edges = np.asarray([edges[i][empties[i]] for i in range(len(edges))])
 
 	# convert from sklearn tree structure to dgl structure
-	idx_dst_pairs = [(idx, dest) for idx, destination in enumerate(edges) for dest in destination if idx != dest] # do not include (a, a) pairs
+	idx_dst_pairs = [(idx, dest) for idx, destination in enumerate(edges) for dest in destination] # do not include (a, a) pairs
 
 
 	distances_per_edge = [distances[idx][np.where(edges[idx] == destination)][0] for idx, destination in idx_dst_pairs]
@@ -52,6 +52,10 @@ def test_reformat(particular_edge):
 		distances, edges = tree.query(positions, k=100, distance_upper_bound=maximum_distance)
 
 		edges, distances = reformat_edges_distances(edges, distances)
+
+		n_zero_distance = np.asarray(distances).shape[0] - np.nonzero(np.asarray(distances))[0].shape[0]
+
+		assert n_zero_distance == len(positions)
 
 
 		origin, dest = edges[particular_edge]
@@ -93,8 +97,67 @@ def hdf52graph(filename, maximum_distance, n_neighbors=None):
 		)
 
 		G.ndata["feat"] = features
-		inv_dist = 1./np.array(distances)
+
+		inv_dist = np.where(np.isfinite(1./np.array(distances)), 1./np.array(distances), 1.)
+		
+		assert np.sum((inv_dist == 1.)) == len(positions)
+
 		G.edata["inv_dist"] = torch.tensor(inv_dist).unsqueeze(-1).float()
+
+		
+		assert np.alltrue(np.isfinite(G.edata["inv_dist"]) )
+
+		#G.edata["dist"] = torch.randn((G.number_of_edges(), 1))
+
+		#labels = torch.tensor(feats["Ngals"][:]).float()
+		#labels = labels.unsqueeze(-1)	# needed for regression
+		labels = feats["Ngals"][:]
+		labels = labels > 0
+		labels = torch.tensor(labels).long()
+		print('Graph generated!')
+
+	return labels, G
+
+def data2graph(filename, maximum_distance, n_neighbors=None):
+
+	with h5py.File(filename, "r+") as feats:
+
+		positions = feats["Pos"][:].clip(min = 0.) / 1000.0  # to Mpc
+
+		tree = cKDTree(positions, boxsize = feats['boxsize'].value/1000.)
+		distances, edges = tree.query(positions, k=100, distance_upper_bound=maximum_distance)
+		edges, distances = reformat_edges_distances(edges, distances)
+
+		src, dst = zip(*edges)
+
+		G = dgl.DGLGraph()
+		G.add_nodes(len(positions))
+		G.add_edges(src, dst)
+
+		features = np.column_stack(
+			[
+				feats["M200c"][:],
+				feats["R200c"][:],
+				feats["N_subhalos"][:],
+				feats["VelDisp"][:],
+				feats["Vmax"][:],
+				feats["Spin"][:],
+				feats["Fsub"][:],
+				feats["x_offset"][:],
+			]
+		)
+
+		G.ndata["feat"] = features
+
+		inv_dist = np.where(np.isfinite(1./np.array(distances)), 1./np.array(distances), 1.)
+		
+		assert np.sum((inv_dist == 1.)) == len(positions)
+
+		G.edata["inv_dist"] = torch.tensor(inv_dist).unsqueeze(-1).float()
+
+		
+		assert np.alltrue(np.isfinite(G.edata["inv_dist"]) )
+
 		#G.edata["dist"] = torch.randn((G.number_of_edges(), 1))
 
 		#labels = torch.tensor(feats["Ngals"][:]).float()
@@ -107,8 +170,20 @@ def hdf52graph(filename, maximum_distance, n_neighbors=None):
 	return labels, G
 
 
+
+
 if __name__ == '__main__':
 
 	hdf5_filename = "/cosma5/data/dp004/dc-cues1/features/halo_features_s99"
 
 	l, G = hdf52graph(hdf5_filename, 5.)
+
+	assert np.alltrue(np.isfinite(G.edata["inv_dist"]) )
+
+
+
+	# origin 0 print all destinations
+	print(G.edges()[1][G.edges()[0] == 0])
+	print(G.edges()[1][G.edges()[0] == 1])
+	print(G.edges()[1][G.edges()[0] == 100])
+	print(G.edges()[1][G.edges()[0] == 2000])
--- models/gcn.py
+++ models/gcn.py
@@ -2,35 +2,38 @@ import dgl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from fully_connected import FullyConnectedModel
 
 
-# Define the message & reduce function
-# NOTE: we ignore the GCN's normalization constant c_ij for this tutorial.
-def gcn_message(edges):
+def gcn_message(edges, weights: bool = False):
 	# The argument is a batch of edges.
 	# This computes a (batch of) message called 'msg' using the source node's feature 'h'.
-	return {"msg": edges.src["h"] }#* edges.data["inv_dist"]}
+	if weights:
+		return {"msg": edges.src["h"] * edges.data["weights"]}
+	else:
+		return {"msg": edges.src["h"] }
 
 
 def gcn_reduce(nodes):
 	# The argument is a batch of nodes.
 	# This computes the new 'h' features by summing received 'msg' in each node's mailbox.
-	# TODO: Check itself is there !
 	return {"h": torch.sum(nodes.mailbox["msg"], dim=1)}
 
 
 # Define the GCNLayer module
 class GCNLayer(nn.Module):
-	def __init__(self, in_feats, out_feats):
+	def __init__(self, in_feats, out_feats, weights = False):
 		super(GCNLayer, self).__init__()
 		self.linear = nn.Linear(in_feats, out_feats)
+		self.weights = weights
 
 	def forward(self, g, inputs):
 		# g is the graph and the inputs is the input node features
 		# first set the node features
 		g.ndata["h"] = inputs
 		# trigger message passing on all edges
-		g.send(g.edges(), gcn_message)
+		weighted_gcn_message = lambda edges: gcn_message(edges, self.weights)
+		g.send(g.edges(), weighted_gcn_message)
 		# trigger aggregation at all nodes
 		g.recv(g.nodes(), gcn_reduce)
 		# get the result node features
@@ -41,36 +44,50 @@ class GCNLayer(nn.Module):
 
 # Define a 2-layer GCN model
 class GCN(nn.Module):
-	def __init__(self, in_feats, conv_hidden_size, embedding_size, readout_hidden_size, num_classes = 1):
-		super(GCN, self).__init__()
-		self.gcn1 = GCNLayer(in_feats, conv_hidden_size)
-		#self.gcn2 = GCNLayer(conv_hidden_size, embedding_size)
+	def __init__(self, input_features: torch.Tensor, 
+			weights: bool = False,
+			conv_hidden_dim: int = 512,
+			embedding_dim: int = 512,
+			readout_n_hidden_layers: int = 2,
+			readout_hidden_dim: int = 512,
+			n_output_dim: int = 2):
 
-		self.fcn1 = nn.Linear(embedding_size, readout_hidden_size)
-		self.fcn2 = nn.Linear(readout_hidden_size, num_classes)
+		super(GCN, self).__init__()
+
+		self.gcn1 = GCNLayer(input_features, conv_hidden_dim, weights)
+		self.gcn2 = GCNLayer(conv_hidden_dim, embedding_dim, weights)
+
+		self.fcn = FullyConnectedModel(n_features = embedding_dim,
+				n_hidden_layers = readout_n_hidden_layers, 
+				n_hidden_dim = readout_hidden_dim, n_output_dim = n_output_dim)
 
 	def forward(self, g, inputs):
 
 		h = self.gcn1(g, inputs)
 		h = torch.relu(h)
-		#h = self.gcn2(g, h)
-		#h = torch.relu(h)
+		h = self.gcn2(g, h)
+		h = torch.relu(h)
 
 		# concatenate embedding and features of the node
 		#h = torch.cat([h, inputs], dim = -1)
 		#h += inputs
 
 		# Readout layers:
-		h = self.fcn1(h)
-		h = F.relu(h)
-		h = self.fcn2(h)
+		h = self.fcn(h)
 		return h
+
+
+# -----------------------------------------------------------------------------
+# TEST MODEL
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
 
 	maximum_distance = 10  # Mpc
 	learning_rate = 1.0e-3
 	num_epochs = 1
+	conv_hidden_dim = 512
+	embedding_dim = 512
 	readout_hidden_size = 512
 	n_classes = 2
 	n_nodes = 3
@@ -100,12 +117,34 @@ if __name__ == '__main__':
 
 	print('Message aggregated at the different nodes')
 
-	conv_hidden_size = G.ndata["h"].shape[-1] 
+	'''
+	conv_hidden_dim = G.ndata["h"].shape[-1] 
 
 	G.send(G.edges(), gcn_message)
 
 	G.recv(G.nodes(), gcn_reduce)
-	print(G.ndata['h'])
+	aggregated_message = G.ndata['h']
+
+	print(f'Aggregated message has shape : {aggregated_message.shape}')
+	'''
+
+	# Compute the forward pass through the model
+	print('Computing forward pass...', end=' ', flush=True)
+
+	net = GCN(G.ndata['h'].shape[-1],
+			False,
+			conv_hidden_dim,
+			embedding_dim,
+			readout_hidden_size,
+			n_output_dim= 2)
+
+	output = net(G, G.ndata['h'])
+
+
+	print('Done!', flush=True)
+	print('Output shape:', output.shape)
+
+
 
 
 

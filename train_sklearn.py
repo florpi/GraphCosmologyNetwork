c407769c9c2c79ae1df78c13bc3572ac7b06496f
Christovis <chrbecker01@gmail.com>
2019-09-25 19:16:37 +0200

    sync

--- train_sklearn.py
+++ train_sklearn.py
@@ -14,10 +14,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import (
-	roc_auc_score,
-	confusion_matrix,
-	mean_squared_error,
-	precision_recall_fscore_support,
+    roc_auc_score,
+    confusion_matrix,
+    mean_squared_error,
+    precision_recall_fscore_support,
 )
 
 from GNN.utils.datautils import get_data, balance_dataset, find_transition_regions
@@ -27,118 +27,119 @@ from sacred import Experiment
 import logging
 
 # -----------------------------------------------------------------------------
-# Loggings 
+# Loggings
 # -----------------------------------------------------------------------------
 tag_datetime = datetime.now().strftime("%H%M_%d%m%Y")
 
-ex = Experiment(
-	"sacred_%s" % tag_datetime,
-	interactive=False,
-)
+ex = Experiment("sacred_%s" % tag_datetime, interactive=False)
 
 logging.basicConfig(
-	filename="experiments/logs/log_%s.log" % tag_datetime,
-	level=logging.INFO,
-	format="%(asctime)s - %(levelname)s - %(message)s",
-	datefmt="%d-%b-%y %H:%M:%S",
+    filename="experiments/logs/log_%s.log" % tag_datetime,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
 )
 
 # -----------------------------------------------------------------------------
-# General Settings/Command-line settings 
+# General Settings/Command-line settings
 # -----------------------------------------------------------------------------
+
 
 @ex.config
 def cfg():
-	# General settings
-	config_gen = {
-		"model": 'rnf', #["rnf, xgboost, lightgbm] 
-		"label": 'dark_or_light', #[dark_or_light, nr_of_galaxies, central_or_satellite, ..]
-		"sampling": 'upsample',  #[upsample, downsample] 
-		"use_pca": True, 
-	} 
-	ex.add_config(config_gen)
-	
+    # General settings
+    config_gen = {
+        "model": "rnf",  # ["rnf, xgboost, lightgbm]
+        "label": "dark_or_light",  # [dark_or_light, nr_of_galaxies, central_or_satellite, ..]
+        "sampling": "upsample",  # [upsample, downsample]
+        "use_pca": True,
+    }
+    ex.add_config(config_gen)
+
+
 # -----------------------------------------------------------------------------
 # MAIN CODE
 # -----------------------------------------------------------------------------
 @ex.automain
 def main(model, label, sampling, use_pca):
 
-	logging.info("")
-	logging.info(f"GROWING TREES")
-	logging.info("")
+    logging.info("")
+    logging.info(f"GROWING TREES")
+    logging.info("")
 
-	# ML-model settings
-	config_file_path = "experiments/config_sklearn/config_%s.json" % model
-	config = load_config(config_file_path=config_file_path)
+    # ML-model settings
+    config_file_path = "experiments/config_sklearn/config_%s.json" % model
+    config = load_config(config_file_path=config_file_path)
 
-	# -------------------------------------------------------------------------
-	# Load and prepare datasets
-	# -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # Load and prepare datasets
+    # -------------------------------------------------------------------------
 
-	# Load dataset
-	output_file = 'merged_dataframe.h5'											 
-	data_path = '/cosma6/data/dp004/dc-cues1/tng_dataframes/'
-	hdf5_filename = data_path + output_file 
-	train, test = get_data(hdf5_filename, label)
+    # Load dataset
+    output_file = "merged_dataframe.h5"
+    data_path = "/cosma6/data/dp004/dc-cues1/tng_dataframes/"
+    hdf5_filename = data_path + output_file
+    train, test = get_data(hdf5_filename, label)
 
-	# Prepare datasets
-	## Balance training set in the transition region
-	center_transition, end_transition = find_transition_regions(train)
+    # Prepare datasets
+    ## Balance training set in the transition region
+    center_transition, end_transition = find_transition_regions(train)
 
-	ex.log_scalar(
-		"The labels before balancing are as follows:", train.labels.value_counts()
-	)
-	train = balance_dataset(
-		train, center_transition, end_transition, sampling
-	)
-	ex.log_scalar(
-		"The labels after balancing are as follows:\n a)",
-		train[train.M200c < center_transition].labels.value_counts(),
-	)
-	ex.log_scalar(
-		"b)",
-		train[
-			(train.M200c > center_transition) & (train.M200c < end_transition)
-		].labels.value_counts(),
-	)
+    ex.log_scalar(
+        "The labels before balancing are as follows:", train.labels.value_counts()
+    )
+    train = balance_dataset(train, center_transition, end_transition, sampling)
+    ex.log_scalar(
+        "The labels after balancing are as follows:\n a)",
+        train[train.M200c < center_transition].labels.value_counts(),
+    )
+    ex.log_scalar(
+        "b)",
+        train[
+            (train.M200c > center_transition) & (train.M200c < end_transition)
+        ].labels.value_counts(),
+    )
 
-	train_features = train.drop(columns="labels")
-	train_labels = train["labels"]
-	
-	test_features = test.drop(columns="labels")
-	test_labels = test["labels"]
+    train_features = train.drop(columns="labels")
+    train_labels = train["labels"]
 
-	## Standarize features
-	scaler = StandardScaler()
-	scaler.fit(train_features)
-	train_features = scaler.transform(train_features)
-	test_features = scaler.transform(test_features)
+    test_features = test.drop(columns="labels")
+    test_labels = test["labels"]
 
-    #if use_pca == True:
-    # Create a PCA object
-    pca = PCA(n_components=len(train_features.columns.values))
-    # Fit the PCA and transform the data
-    train_features = pca.fit_transform(train_features.values)
+    ## Standarize features
+    scaler = StandardScaler()
+    scaler.fit(train_features)
+    train_features = scaler.transform(train_features)
+    test_features = scaler.transform(test_features)
 
-	# -------------------------------------------------------------------------
-	# Set-up and Run random-forest (RNF) model
-	# -------------------------------------------------------------------------
+	if use_pca == True:
+	    # Create a PCA object
+	    pcas = PCA(n_components=len(train_features.columns.values))
+	    train_feat_pca = pcas.fit_transform(train_features.values)
 
-	rf = RandomForestClassifier(**config["model"]["parameters"])
-	rf.fit(train_features, train_labels)
+	    # convert array back to dataframe
+	    train_features = pd.DataFrame(
+	        data=train_feat_pca,
+	        index=np.arange(train_feat_pca.shape[0]),
+	        columns=["PCA_%d" % dd for dd in range(train_feat_pca.shape[1])],
+	    )
 
-	# Run RNF
-	test_pred = rf.predict(test_features)
+    # -------------------------------------------------------------------------
+    # Set-up and Run random-forest (RNF) model
+    # -------------------------------------------------------------------------
 
-	# Save results
-	fname_out = "./outputs/train_%s_%s" % (model, tag_datetime)
-	train_labels.to_hdf(fname_out, key='df', mode='w')
-	
-	fname_out = "./outputs/test_%s_%s" % (model, tag_datetime)
-	test_labels.to_hdf(fname_out, key='df', mode='w')
+    rf = RandomForestClassifier(**config["model"]["parameters"])
+    rf.fit(train_features, train_labels)
 
-	fname_out = "./outputs/predic_%s_%s" % (model, tag_datetime)
-	np.save(fname_out, test_pred)
+    # Run RNF
+    test_pred = rf.predict(test_features)
 
- 
+    # Save results
+    fname_out = "./outputs/train_%s_%s" % (model, tag_datetime)
+    train_labels.to_hdf(fname_out, key="df", mode="w")
+
+    fname_out = "./outputs/test_%s_%s" % (model, tag_datetime)
+    test_labels.to_hdf(fname_out, key="df", mode="w")
+
+    fname_out = "./outputs/predic_%s_%s" % (model, tag_datetime)
+    np.save(fname_out, test_pred)
--- utils/datautils.py
+++ utils/datautils.py
@@ -6,61 +6,85 @@ from scipy.stats import binned_statistic
 import h5py
 import pandas as pd
 
-#from imblearn.over_sampling import SMOTE
+# from imblearn.over_sampling import SMOTE
 from scipy.interpolate import interp1d
 
 
 def get_data(hdf5_filename: str, arg_label: str):
+    """
 	"""
-	"""
-	df = pd.read_hdf(hdf5_filename, key='df', mode='r')
+    df = pd.read_hdf(hdf5_filename, key="df", mode="r")
 
-	# Chose label
-	if arg_label == "dark_or_light":
-		df['labels'] = df.N_gals > 0
-		df = df.drop(columns = ['N_gals', 'ID_HYDRO', 'ID_DMO', 'M200_HYDRO', 'M_stars', 'x_hydro', 'y_hydro', 'z_hydro'])
-	elif arg_label == "nr_of_galaxies":
-		df['labels'] = df.N_gals
-		df = df.drop(columns = ['N_gals', 'ID_HYDRO', 'ID_DMO', 'M200_HYDRO', 'M_stars', 'x_hydro', 'y_hydro', 'z_hydro'])
-	elif arg_label == "both":
-		df['labels'] = df.N_gals > 0
+    # Chose label
+    if arg_label == "dark_or_light":
+        df["labels"] = df.N_gals > 0
+        df = df.drop(
+            columns=[
+                "N_gals",
+                "ID_HYDRO",
+                "ID_DMO",
+                "M200_HYDRO",
+                "M_stars",
+                "x_hydro",
+                "y_hydro",
+                "z_hydro",
+            ]
+        )
+    elif arg_label == "nr_of_galaxies":
+        df["labels"] = df.N_gals
+        df = df.drop(
+            columns=[
+                "N_gals",
+                "ID_HYDRO",
+                "ID_DMO",
+                "M200_HYDRO",
+                "M_stars",
+                "x_hydro",
+                "y_hydro",
+                "z_hydro",
+            ]
+        )
+    elif arg_label == "both":
+        df["labels"] = df.N_gals > 0
 
-	# Test, train, validation split
-	train_idx, test_idx, val_idx = _train_test_val_split(
-		df.labels.values.shape[0], train_size=0.5
-	)
-	test_idx = np.concatenate((test_idx, val_idx))	# TODO: only temporary
+    # Test, train, validation split
+    train_idx, test_idx, val_idx = _train_test_val_split(
+        df.labels.values.shape[0], train_size=0.5
+    )
+    test_idx = np.concatenate((test_idx, val_idx))  # TODO: only temporary
 
-	train = df.iloc[train_idx]
-	test = df.iloc[test_idx]
+    train = df.iloc[train_idx]
+    test = df.iloc[test_idx]
 
-	return train, test
+    return train, test
 
 
 def _train_test_val_split(n_nodes, train_size=0.7):
-	"""
+    """
 	internal function indicated by leading _
 	"""
-	np.random.seed(0)
+    np.random.seed(0)
 
-	test_val_idx = np.random.choice(
-		n_nodes, size=int((1.0 - train_size) * n_nodes), replace=False
-	)
-	val_idx = np.random.choice(
-		test_val_idx, size=int(0.5 * len(test_val_idx)), replace=False
-	)
+    test_val_idx = np.random.choice(
+        n_nodes, size=int((1.0 - train_size) * n_nodes), replace=False
+    )
+    val_idx = np.random.choice(
+        test_val_idx, size=int(0.5 * len(test_val_idx)), replace=False
+    )
 
-	test_idx = np.array(list(set(test_val_idx) - set(val_idx)))
-	train_idx = np.array(list(set(np.arange(0, n_nodes)) - set(test_val_idx)))
+    test_idx = np.array(list(set(test_val_idx) - set(val_idx)))
+    train_idx = np.array(list(set(np.arange(0, n_nodes)) - set(test_val_idx)))
 
-	assert n_nodes == len(val_idx) + len(test_idx) + len(train_idx)
+    assert n_nodes == len(val_idx) + len(test_idx) + len(train_idx)
 
-	return train_idx, test_idx, val_idx
+    return train_idx, test_idx, val_idx
+
 
 def find_transition_regions(df: pd.DataFrame):
-	"""
+    """
 
-	Function to find two masses: where half the haloes are luminous, and where all haloes are luminous
+	Function to find two masses: where half the haloes are luminous,
+	and where all haloes are luminous
 
 	Args:
 		df: dataframe containing masses and wheather luminous or dark
@@ -69,103 +93,69 @@ def find_transition_regions(df: pd.DataFrame):
 		mass_end: mass at which 100% of haloes are luminous.
 
 	"""
-	nbins= 15
-	bins = np.logspace(np.log10(np.min(df.M200c)),
-			                   12.5, nbins+1)
+    nbins = 15
+    bins = np.logspace(np.log10(np.min(df.M200c)), 12.5, nbins + 1)
 
-	nluminous, mass_edges, _ = binned_statistic(df.M200c, df.labels, statistic = 'mean', bins = bins)
+    nluminous, mass_edges, _ = binned_statistic(
+        df.M200c, df.labels, statistic="mean", bins=bins
+    )
 
-	interpolator = interp1d(nluminous, (mass_edges[1:]+mass_edges[:-1])/2.)
+    interpolator = interp1d(nluminous, (mass_edges[1:] + mass_edges[:-1]) / 2.0)
 
-	mass_center = interpolator(0.5)
+    mass_center = interpolator(0.5)
 
-	mass_end = ((mass_edges[1:] + mass_edges[:-1])/2.)[nluminous == 1.][0]
+    mass_end = ((mass_edges[1:] + mass_edges[:-1]) / 2.0)[nluminous == 1.0][0]
 
-	return mass_center, mass_end
-
+    return mass_center, mass_end
 
 
 def balance_dataset(
-	df, center_transition: float, end_transition: float, arg_sampling: str
-): 
-    # TODO: fix balance parameters; return none 
+    df, center_transition: float, end_transition: float, arg_sampling: str
+):
+    # TODO: fix balance parameters; return none
     df_sample = _balance_df_given_mass(
         df, "labels", 0.0, center_transition, 0, 1, mode=arg_sampling
     )
     df_sample = _balance_df_given_mass(
-        df_sample,
-        "labels",
-        center_transition,
-        end_transition,
-        1,
-        0,
-        mode=arg_sampling,
+        df_sample, "labels", center_transition, end_transition, 1, 0, mode=arg_sampling
     )
 
-    return df_sample 
-
-
-def _find_center_of_balance(df):
-    """
-    """
-
-    # bin data
-    nbins= 15
-    bins = np.logspace(
-        np.log10(np.min(df.M200c)),
-        12.5,
-        nbins+1,
-    )
-    
-    # nr. of luminous galaxies
-    nluminous, edges, _ = binned_statistic(
-        df.M200c,
-        df.labels, 
-        statistic='mean',
-        bins=bins,
-    )
-
-    # Find x for which y = 0.5
-    interpolator = interp1d(nluminous, (edges[1:]+edges[:-1])/2.)
-    centre = interpolator(0.5)
-    end = ((edges[1:]+edges[:-1])/2.)[nluminous == 1.][0]
-    
-    return centre, end
+    return df_sample
 
 
 def _balance_df_given_mass(
-	df, labels_name, minimum_mass, maximum_mass, majority, minority, mode="upsample"
+    df, labels_name, minimum_mass, maximum_mass, majority, minority, mode="upsample"
 ):
-	"""
+    """
 	internal function indicated by leading _
 	"""
-	mass_threshold = (df.M200c > minimum_mass) & (df.M200c < maximum_mass)
+    mass_threshold = (df.M200c > minimum_mass) & (df.M200c < maximum_mass)
 
-	df_M = df[mass_threshold]
+    df_M = df[mass_threshold]
 
-	df_M_majority = df_M[df_M[labels_name] == majority]
-	df_M_minority = df_M[df_M[labels_name] == minority]
+    df_M_majority = df_M[df_M[labels_name] == majority]
+    df_M_minority = df_M[df_M[labels_name] == minority]
 
-	if mode == "upsample":
-		df_M_minority_upsampled = resample(
-			df_M_minority,
-			replace=True,
-			n_samples=df_M_majority.shape[0],
-			random_state=123,
-		)
+    if mode == "upsample":
+        df_M_minority_upsampled = resample(
+            df_M_minority,
+            replace=True,
+            n_samples=df_M_majority.shape[0],
+            random_state=123,
+        )
 
-		return pd.concat([df_M_majority, df_M_minority_upsampled, df[~mass_threshold]])
+        return pd.concat([df_M_majority, df_M_minority_upsampled, df[~mass_threshold]])
 
-	elif mode == "downsample":
-		df_M_majority_downsampled = resample(
-			df_M_majority,
-			replace=False,
-			n_samples=df_M_minority.shape[0],
-			random_state=123,
-		)
-		return pd.concat(
-			[df_M_majority_downsampled, df_M_minority, df[~mass_threshold]]
-		)
+    elif mode == "downsample":
+        df_M_majority_downsampled = resample(
+            df_M_majority,
+            replace=False,
+            n_samples=df_M_minority.shape[0],
+            random_state=123,
+        )
+        return pd.concat(
+            [df_M_majority_downsampled, df_M_minority, df[~mass_threshold]]
+        )
 
-	# elif mode == 'smote':
-	#	sm = SMOTE(random_state = 12, ratio = 1.)
+    # elif mode == 'smote':
+    # 	sm = SMOTE(random_state = 12, ratio = 1.)
